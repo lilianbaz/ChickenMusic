26f533087c062748a0bef3febcaad1a0deafd30c
Lilian BAZILLE <lilian@bazille.me>
2021-04-14 17:59:39 +0200

    Typo

--- music.php
+++ music.php
@@ -15,7 +15,7 @@ do	{
 	# Year generation
 	$randYear = random_int($Ystart, $Yend);
 
-	# Month generatoin
+	# Month generation
 	if($randYear == $Ystart)	{
 		$randMonth = random_int($Mstart, 13);
 	}
@@ -40,27 +40,3 @@ do	{
 } while(!$found);
 
 $url = $baseImgUrl . $rand . ".jpg";
-
-/*
-?>
-
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<title>Un peu de musique mon con ?</title>
-		<style>
-			.rainbow {
-  				background-image: -webkit-gradient( linear, left top, right top, color-stop(0, #f22), color-stop(0.15, #f2f), color-stop(0.3, #22f), color-stop(0.45, #2ff), color-stop(0.6, #2f2),color-stop(0.75, #2f2), color-stop(0.9, #ff2), color-stop(1, #f22) );
-  				background-image: gradient( linear, left top, right top, color-stop(0, #f22), color-stop(0.15, #f2f), color-stop(0.3, #22f), color-stop(0.45, #2ff), color-stop(0.6, #2f2),color-stop(0.75, #2f2), color-stop(0.9, #ff2), color-stop(1, #f22) );
-  				color:transparent;
-  				-webkit-background-clip: text;
-  				background-clip: text;
-			}
-		</style>
-	</head>
-	<body>
-
-		<?= "<div style=\"text-align:center\"><img style=\"width:600px\" src=\"$baseImgUrl$rand.jpg\"></div>"; ?>
-
-	</body>
-</html>*/
